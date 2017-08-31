c00fd066d7ae8d2054a51d4f11a463799ec68161
bidahor13@wou.edu <bidahor13@wou.edu>
2017-08-31 12:32:27 -0700

    test

---
+++ test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Add up my name :)
+
+	fmt.Println("Baba" + "tunde" + "{" + "Coding Sensei" + "}")
+
+	fmt.Println("1+1 =", 1+1)
+
+	fmt.Println("tunde =", 22/7)
+
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
---
+++ variables.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var greeting = "Welcome"
+	fmt.Println(greeting)
+
+	var fName, lName, zip = "Tunde", "Idahor", 97000
+	fmt.Println(fName, lName, zip)
+
+	var country = "USA"
+	fmt.Println(country)
+
+	var zipCode = 2017
+	fmt.Println(zipCode)
+
+	location := "Salem, Oregon"
+	fmt.Println(location)
+
+	Age := 100
+	fmt.Println(Age)
+
+}
