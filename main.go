c9d9376e37842b8e45b99bd35430dcfff6fe7e03
bidahor13@wou.edu <bidahor13@wou.edu>
2018-12-11 15:50:06 -0800

    added some french

--- main.go
+++ main.go
@@ -15,16 +15,21 @@ import "fmt"
 
 const lang_1 = "Ghana"
 const lang_2 = "Bini"
+const lang_3 = "French"
 
 func main(){
+    
     fmt.Println("Welcome to my Gopher domain.")
     var ghMessage = "Akwaaba"
     var biMessage = "koyoo"
+    var frMessage = "Bonjour"
     fmt.Println("The word",ghMessage, "originated from" ,lang_1,".")
-    fmt.Println("The phrase",biMessage,"originated from", lang_2,".")
+    fmt.Println("The phrase",biMessage,"originated from",lang_2,".")
+    fmt.Printf("Language of origin : %s, %s, %q " , lang_1, lang_2, lang_3)
+    fmt.Printf("%s\n", frMessage)
     
-    printVariables()
-    showData()
-    Sensei()
+    // printVariables()
+    // showData()
+    // Sensei()
 }
 
