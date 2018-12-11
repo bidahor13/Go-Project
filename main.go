40e39ec37848acb48bc01e9568aeaa317a33efcf
bidahor13@wou.edu <bidahor13@wou.edu>
2018-12-11 04:47:37 -0800

    added a new message

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
+
+
 package main
 
 import "fmt"
 
+const language = true
+
 func main(){
     fmt.Println("Welcome to my Gopher domain.")
+    var message = "Akwaaba"
+    fmt.Println(message, language)
     
 }
